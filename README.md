2183c81908f9844b45516e4d59a05d95051ac9cc
persmule <persmule@gmail.com>
2017-04-17 22:24:18 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# psiplus-otr-g2n
+A small script to help migrate psi-plus' otr key file.
---
+++ psiplus-otr-g2n.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Copyright (c) 2017 Persmule. All Rights Reserved.
+#
+# The library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+# 
+# The library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA 
+
+
+
+g2n() {
+    xmlstarlet sel -N x="http://psi-im.org/options" -t -v "/x:accounts/x:accounts/*[x:id=\"${1}\"]/x:jid" "${2}";
+}
+
+getguid() {
+    grep name "${1}" | sed -e 's/^(name \"//g' -e 's/\")$//g';
+}
+
+mkregex() {
+    printf 's/%s/%s/g' ${1} $(g2n ${1} "${2}");
+}
+
+if [ ${#} -lt 2 ]; then
+   echo "usage: ${0} <psi+ otr key file> <account xml file> <converted key file>"
+   exit 0
+elif which xmlstarlet > /dev/null; then
+    l_guid=$(getguid "${1}");
+    cp "${1}" "${3}"
+    for g in ${l_guid};
+    do {
+	sed -i "$(mkregex ${g} ${2})" "${3}"
+    } 
+    done
+else
+    echo "This script needs xmlstarlet. Please install it first!"
+    exit 1
+fi
